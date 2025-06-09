18fde1f16b27ff0684f65d75866824e5d88b044b
YogaPrakashA <asprakash1208@gmail.com>
2025-06-09 19:52:17 +0530

    findPalindrome

--- src/com/leetcode/array/FindMissingElement.java
+++ src/com/leetcode/array/FindMissingElement.java
@@ -1,4 +1,24 @@
 package com.leetcode.array;
 
 public class FindMissingElement {
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+        int lastElementOfArray = nums[nums.length - 1];
+        System.out.println(findMissingElement(nums, lastElementOfArray));
+
+    }
+
+    private static int findMissingElement(int[] nums, int lastElementOfArray) {
+        int sumOfNums = 0;
+        for (int arr : nums)
+            sumOfNums += arr;
+
+        int missingNum = lastElementOfArray * (lastElementOfArray + 1) / 2;
+
+        missingNum = missingNum - sumOfNums;
+
+        return missingNum == 0 ? lastElementOfArray + 1 : missingNum;
+
+
+    }
 }
---
+++ src/com/leetcode/string/FindPalindrome.java
@@ -0,0 +1,43 @@
+package com.leetcode.string;
+
+public class FindPalindrome {
+//     find palindrome if extra character occured above one that is not palindrome
+        /*input = madam   output = true
+         input = madham  output = true
+         input = madhiam output  = false
+
+         Source : paypal
+*/
+    public static void main(String[] args) {
+            String input = "madam";
+            System.out.println(findPalindrome(input));
+    }
+
+
+    private static boolean findPalindrome(String s) {
+
+        int left = 0, right = s.length() - 1;
+        boolean response = false;
+            while (left < right) {
+                if (s.charAt(left) != s.charAt(right)) {
+                    if(response)
+                        return false;
+
+                    if(s.charAt(left) == s.charAt(right-1))
+                            right--;
+                    else
+                        left++;
+
+                    response = true;
+
+                }else{
+                    right--;
+                    left++;
+                }
+            }
+
+            return true;
+
+
+    }
+}
