db1e36e92aac5c7da901d12477ddedd91afe44f1
YogaPrakashA <asprakash1208@gmail.com>
2025-06-09 21:51:46 +0530

    FindLinkedListInfinityLoop

---
+++ src/com/leetcode/linkedlist/FindLinkedListLoop.java
@@ -0,0 +1,36 @@
+package com.leetcode.linkedlist;
+
+public class FindLinkedListLoop {
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        node.next.next.next = new Node(4);
+
+        System.out.println(findLinkedListLoop(node));
+    }
+
+    public static Boolean findLinkedListLoop(Node head) {
+
+        if (head == null)
+            return false;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (slow != null && fast != null) {
+
+            if(fast.next == null)
+                return false;
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast)
+                return true;
+
+        }
+    return false;
+    }
+
+}
